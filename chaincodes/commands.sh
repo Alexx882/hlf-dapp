b0eaeade6429d8695b1623f02808fb245f86391a
Alexander Lercher <allercher@edu.aau.at>
2019-12-16 16:30:31 +0100

    updated commands

--- chaincodes/commands.sh
+++ chaincodes/commands.sh
@@ -1,6 +1,8 @@
 peer chaincode install -l node -p chaincodes/filescc/ -n test_cc -v 1
 
-peer chaincode instantiate -l node -o orderer:7050 -c '{"Args":[]}' -n test_cc -C myc -v 1
+peer chaincode instantiate -l node -o orderer:7050 -c '{"Args":["initLedger"]}' -n test_cc -C myc -v 1
 
 peer chaincode invoke -C myc -n test_cc -c '{"Args":["registerUser","Alex","10","1","1"]}'
-peer chaincode invoke -C myc -n test_cc -c '{"Args":["registerFile","file","10","1","1"]}'
+peer chaincode query -C myc -n test_cc -c '{"Args":["getUser","Alex"]}'
+
+# peer chaincode query -C myc -n test_cc -c '{"Args":["registerFile","file","10","1","1"]}'
