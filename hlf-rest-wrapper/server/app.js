92ce9f1e052601347505ddb36953f01dea4dc2d0
Michael <michael@marolt.com>
2020-01-18 20:17:46 +0100

    hlf-rest-wrapper

--- hlf-rest-wrapper/server/app.js
+++ hlf-rest-wrapper/server/app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var fileRouter = require('./routes/file');
 var usersRouter = require('./routes/users');
+var saleRouter = require('./routes/sale')
 
 var app = express();
 
@@ -19,23 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/file', fileRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/sale', saleRouter)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- hlf-rest-wrapper/server/routes/file.js
+++ hlf-rest-wrapper/server/routes/file.js
@@ -8,7 +8,7 @@ const path = require('path');
 const ccpPath = path.resolve(__dirname, '..', "connection.json");
 
 
-router.post('/registerUser',async (req,res) => {
+router.post('/registerFile',async (req,res) => {
   try {
     
     // Create a new file system based wallet for managing identities.
@@ -44,11 +44,11 @@ router.post('/registerUser',async (req,res) => {
     // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
     console.log(req.body);
     
-    const result = await contract.submitTransaction('FabUser:registerUser', req.body.username, req.body.credit,req.body.tradingType, req.body.admin);
+    const result = await contract.submitTransaction('FabFile:registerFile', req.body.filename, req.body.owner, req.body.type, req.body.price, req.body.available, req.body.hash);
     console.log(`Transaction has been submitted, result is: ${result.toString()}`);
-    const out = await contract.evaluateTransaction('FabUser:getUser',req.body.username);
+    const out = await contract.evaluateTransaction('FabFile:getFile',req.body.filename);
     if (!out) {
-      return res.status(500).send("Failed to register User")
+      return res.status(500).send("Failed to register File")
     }
     res.status(200).json(JSON.parse(out.toString("utf8")));
     await gateway.disconnect();
@@ -58,6 +58,67 @@ router.post('/registerUser',async (req,res) => {
     res.status(400).json({error: error});
   } 
 })
+
+
+router.post('/buyFile',async (req,res) => {
+  try {
+    
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), 'wallet');
+    console.log(walletPath);
+    
+    const wallet = new FileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists('filecc-user');
+    if (!userExists) {
+        console.log('An identity for the user "filecc-user" does not exist in the wallet');
+        console.log('Run the registerUser.js application before retrying');
+        return;
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccpPath, { wallet, identity: 'filecc-user', discovery: { enabled: false} });
+
+    
+      
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+
+    // Get the contract from the network.
+    const contract = network.getContract('fabfile');
+
+    // Evaluate the specified transaction.
+    // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+    // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+    
+    const result = await contract.submitTransaction('FabFile:buyFile', req.body.filename, req.body.buyername);
+    console.log(`Transaction has been submitted, result is: ${result.toString()}`);
+    let out = await (await contract.evaluateTransaction('FabSale:getAllSales')).toString("utf8");
+    if (!out) {
+      return res.status(500).send("Failed to register File")
+    }
+    out = JSON.parse(out)
+  
+    out.forEach(element => {
+      console.log(element);
+      
+      if (element.Record.filename == req.body.filename && element.Record.buyername == req.body.buyername) {
+        res.status(200).json(element);
+      }
+    });
+    
+    await gateway.disconnect();
+
+  } catch (error) {
+    console.error('Failed to evaluate transaction: ', error);
+    res.status(400).json({error: error});
+  } 
+})
+
 
 router.patch('/',async (req,res) => {
   try {
@@ -90,19 +151,19 @@ router.patch('/',async (req,res) => {
     // Get the contract from the network.
     const contract = network.getContract('fabfile');
 
-    if (req.body.credit) {
-      const result = await contract.submitTransaction("FabUser:updateUserCredit",req.body.username, req.body.credit);
+    if (req.body.available) {
+      const result = await contract.submitTransaction("FabFile:updateFileAvailability",req.body.filename, req.body.available);
       
-      const out = await contract.evaluateTransaction('FabUser:getUser',req.body.username);
+      const out = await contract.evaluateTransaction('FabFile:getFile',req.body.filename);
     if (!out) {
-      return res.status(500).send("Failed to register User")
+      return res.status(500).send("Failed to update File")
     }
     res.status(200).json(JSON.parse(out.toString("utf8")));
-    } else if (req.body.tradingType) {
-      const result = await contract.submitTransaction('FabUser:updateUserTradingType',req.body.username,req.body.tradingType);
-      const out = await contract.evaluateTransaction('FabUser:getUser',req.body.username);
+    } else if (req.body.price) {
+      const result = await contract.submitTransaction('FabFile:updateFilePrice',req.body.filename,req.body.price);
+      const out = await contract.evaluateTransaction('FabFile:getFile',req.body.filename);
     if (!out) {
-      return res.status(500).send("Failed to register User")
+      return res.status(500).send("Failed to update File")
     }
     res.status(200).json(JSON.parse(out.toString("utf8")));
     } else {
@@ -116,7 +177,7 @@ router.patch('/',async (req,res) => {
   }
 })
 
-router.get('/getUser', async (req,res) => {
+router.get('/getFile', async (req,res) => {
   try {
     
     // Create a new file system based wallet for managing identities.
@@ -150,7 +211,7 @@ router.get('/getUser', async (req,res) => {
     // Evaluate the specified transaction.
     // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
     // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-    const result = await contract.evaluateTransaction('FabUser:getUser',req.body.username);
+    const result = await contract.evaluateTransaction('FabFile:getFile',req.body.filename);
     console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
     res.status(200).json(JSON.parse(result.toString("utf8")));
     await gateway.disconnect();
@@ -161,7 +222,8 @@ router.get('/getUser', async (req,res) => {
   } 
 })
 
-router.get('/getAllUsers', async (req,res) => {
+
+router.get('/getAllFiles', async (req,res) => {
   try {
     
     // Create a new file system based wallet for managing identities.
@@ -195,7 +257,7 @@ router.get('/getAllUsers', async (req,res) => {
     // Evaluate the specified transaction.
     // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
     // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
-    const result = await contract.evaluateTransaction('FabUser:getAllUsers');
+    const result = await contract.evaluateTransaction('FabFile:getAllFiles');
     console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
     res.status(200).json(JSON.parse(result.toString("utf8")));
     await gateway.disconnect();
@@ -207,8 +269,9 @@ router.get('/getAllUsers', async (req,res) => {
 })
 
 router.use('*', function(req, res, next) {
-  res.send('wrong URL');
+  res.status(404).send('wrong URL');
 });
 
-module.exports = router;
 
+
+module.exports = router;
